c97833699ab84fadf59214e78d0790639bc0c461
Arian <arianhosseini@outlook.com>
2022-08-16 23:39:25 +0430

    App.js Component created

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+	// Our react code goes in here!
+	render() {
+		return (
+			<div className="app">
+				<h1>Hello, World!</h1>
+			</div>
+		);
+	}
+}
+
+export default App;
