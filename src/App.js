d583bc925fb54d0bca25a8f1e7ca0091c97c4c32
Arian Hosseini <arianhosseini@outlook.com>
2022-10-27 20:36:28 +0330

    feat: added web3

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Web3 from "web3";
 
 import { Navbar } from "./components";
 
@@ -7,6 +8,25 @@ import "./App.css";
 const App = () => {
 	// The Ethereum Acccount address of our customer (investor, client)
 	const [account, setAccount] = useState("0x0");
+
+	const loadWeb3 = async () => {
+		if (window.ethereum) {
+			window.web3 = new Web3(window.ethereum);
+			await window.ethereum.enable();
+		} else if (window.web3) {
+			window.web3 = new Web3(window.web3.currenctProvider);
+		} else {
+			alert("No Ethereum browser detected! You can check out MetaMask!");
+		}
+	};
+
+	useEffect(() => {
+		const fetchData = async () => {
+			await loadWeb3();
+		};
+
+		fetchData();
+	}, []);
 
 	return (
 		<div>
