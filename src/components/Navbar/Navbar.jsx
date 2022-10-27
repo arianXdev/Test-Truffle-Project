d2e6d933109494d60b825a505cd5de470364a3e9
Arian Hosseini <arianhosseini@outlook.com>
2022-10-27 17:57:49 +0330

    feat: created Navbar Component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,21 @@
 import "./Navbar.css";
 
 const Navbar = () => {
-	return <div>I am a Navbar.</div>;
+	return (
+		<nav className="navbar">
+			<div className="container">
+				<div className="navbar__content">
+					<h1 className="title">Decentraliezd Application</h1>
+
+					<ul>
+						<li>
+							<small>ACCOUNT NUMBER: </small>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+	);
 };
 
 export default Navbar;
